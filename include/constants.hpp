ddf4e56a8b00510568a3ea684c535cb02a0525ef
polinka <zagigalka_60@mail.ru>
2021-05-31 21:43:27 +0300

    1

---
+++ include/constants.hpp
@@ -0,0 +1,17 @@
+//
+// Created by polinka on 20.04.2021.
+//
+
+#ifndef LAB_10_KV_STORAGE_CONSTANTS_HPP
+#define LAB_10_KV_STORAGE_CONSTANTS_HPP
+
+const std::string CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuvwxyz";
+static const size_t KEY_LENGTH = 1;
+static const size_t VALUE_LENGTH = 1;
+static const size_t FAMILY_NAME_LENGTH = 1;
+static const size_t FAMILY_COUNT = 3;
+static const size_t STR_COUNT = 5;
+static const size_t DEFAULT_THREAD_HASH = 2;
+
+#endif  // LAB_10_KV_STORAGE_CONSTANTS_HPP
+
